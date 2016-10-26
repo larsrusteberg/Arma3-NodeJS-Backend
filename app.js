195c52929068a4ebf28860e3ae48160880091d9a
Lars Rusteberg <lars.rusteberg@gmail.com>
2016-10-26 08:40:04 +0200

    added package.json file and added an example http server

---
+++ app.js
@@ -0,0 +1,9 @@
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+res.setHeader('Content-Type', 'text/plain');
+res.end('Hello World\n');
+});
+
+server.listen(0, '0.0.0.0', () => {
+  console.log('Node.js app is running...');
+});
