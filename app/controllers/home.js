6ecea0dd048a5d9f0641973b25195c441c3211ea
Lars Rusteberg <lars.rusteberg@gmail.com>
2016-10-26 10:06:15 +0200

    added route which shows all files

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -1,5 +1,6 @@
 var express = require('express'),
-  router = express.Router();
+  router = express.Router(),
+  walkUtil = require('../utils/walk');
 
 module.exports = function (app) {
   app.use('/', router);
@@ -8,4 +9,17 @@ module.exports = function (app) {
 router.get('/', function (req, res, next) {
   res.contentType("application/json");
   res.send('{}')
+});
+
+router.get('/files', function (req, res, next) {
+  walkUtil.walk('../calderia-mods', function(err, results) {
+    var pathCorrectedResults = [];
+    if (err) throw err;
+    res.contentType("application/json");
+    results.forEach(function (result) {
+      var correctedPath = result.replace('../calderia-mods/', '');
+      pathCorrectedResults.push(correctedPath);
+    });
+    res.send(JSON.stringify(pathCorrectedResults))
+  });
 });
