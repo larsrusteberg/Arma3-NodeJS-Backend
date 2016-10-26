e35b0df23fc37e6d7d9b1fa13554760848dd90a6
Lars Rusteberg <lars.rusteberg@gmail.com>
2016-10-26 11:04:10 +0200

    sorting files into a Map

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -11,15 +11,35 @@ router.get('/', function (req, res, next) {
   res.send('{}')
 });
 
+function strMapToObj(strMap) {
+  let obj = Object.create(null);
+  for (let [k,v] of strMap) {
+    // We don’t escape the key '__proto__'
+    // which can cause problems on older engines
+    obj[k] = v;
+  }
+  return obj;
+}
+
 router.get('/files', function (req, res, next) {
   walkUtil.walk('../calderia-mods', function(err, results) {
+    let myMap = new Map();
     var pathCorrectedResults = [];
     if (err) throw err;
     res.contentType("application/json");
     results.forEach(function (result) {
       var correctedPath = result.replace('../calderia-mods/', '');
-      pathCorrectedResults.push(correctedPath);
+      var length = correctedPath.indexOf('/');
+      var modName = correctedPath.substr(0, length);
+      if(myMap.has(modName)) {
+        myMap.get(modName).push(correctedPath)
+      } else {
+        var array = [];
+        array.push(correctedPath);
+        myMap.set(modName, array)
+      }
+      // pathCorrectedResults.push(modName);
     });
-    res.send(JSON.stringify(pathCorrectedResults))
+    res.send(JSON.stringify(strMapToObj(myMap)))
   });
 });
