4815fea8e742ece449aaf3e0e9a5ef827fe066a9
Lars Rusteberg <lars.rusteberg@gmail.com>
2016-10-28 10:26:51 +0200

    added memcached caching

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -4,6 +4,10 @@ var express = require('express'),
   md5 = require('md5-file'),
   fs = require('fs');
 
+var MemcachePlus = require('memcache-plus');
+var port = process.env.MEMCACHED_PORT || 11211;
+var client = new MemcachePlus();
+
 module.exports = function (app) {
   app.use('/', router);
 };
@@ -25,33 +29,43 @@ function strMapToObj(strMap) {
 
 router.get('/files', function (req, res, next) {
   walkUtil.walk('../calderia-mods', function(err, results) {
-    let myMap = new Map();
-    var pathCorrectedResults = [];
-    if (err) throw err;
-    res.contentType("application/json");
-    results.forEach(function (result) {
-      let correctedPath = result.replace('../calderia-mods/', '');
-      let length = correctedPath.indexOf('/');
-      let modName = correctedPath.substr(0, length);
-      let fileObj = {};
-      let stats = fs.statSync(result);
-      if(myMap.has(modName)) {
-        fileObj.path = correctedPath;
-        fileObj.md5 = md5.sync(result);
-        fileObj.size = stats.size;
-        fileObj.mtime = stats.mtime;
-        myMap.get(modName).push(fileObj)
+    var cacheKey = "calderia_mods_cache";
+    client.get(cacheKey).then(function (value) {
+      if(value) {
+        res.contentType("application/json");
+        res.send(value)
       } else {
-        let array = [];
-        fileObj.path = correctedPath;
-        fileObj.md5 = md5.sync(result);
-        fileObj.size = stats.size;
-        fileObj.mtime = stats.mtime;
-        array.push(fileObj);
-        myMap.set(modName, array)
+        let myMap = new Map();
+        var pathCorrectedResults = [];
+        if (err) throw err;
+        res.contentType("application/json");
+        results.forEach(function (result) {
+          let correctedPath = result.replace('../calderia-mods/', '');
+          let length = correctedPath.indexOf('/');
+          let modName = correctedPath.substr(0, length);
+          let fileObj = {};
+          let stats = fs.statSync(result);
+          if(myMap.has(modName)) {
+            fileObj.path = correctedPath;
+            fileObj.md5 = md5.sync(result);
+            fileObj.size = stats.size;
+            fileObj.mtime = stats.mtime;
+            myMap.get(modName).push(fileObj)
+          } else {
+            let array = [];
+            fileObj.path = correctedPath;
+            fileObj.md5 = md5.sync(result);
+            fileObj.size = stats.size;
+            fileObj.mtime = stats.mtime;
+            array.push(fileObj);
+            myMap.set(modName, array)
+          }
+          // pathCorrectedResults.push(modName);
+        });
+        var data = JSON.stringify(strMapToObj(myMap));
+        client.set(cacheKey, data, 864000);
+        res.send(data)
       }
-      // pathCorrectedResults.push(modName);
     });
-    res.send(JSON.stringify(strMapToObj(myMap)))
   });
 });
